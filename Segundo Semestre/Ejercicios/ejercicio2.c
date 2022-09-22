d20bd16cc8779f48d5332f4e3d60d51050188759
Saul Muñoz <sam.pedreros14@gmail.com>
2022-09-22 17:29:14 -0300

    Un poco mejor ordenado

--- Segundo Semestre/Ejercicios/ejercicio2.c
+++ Segundo Semestre/Ejercicios/ejercicio2.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
-#include <math.h>
+
 
 int x1, x2, y1, y2, resultado;
 
 int main()
 {
     printf("Ingrese 4 puntos del plano cartesiano, tales que sea: x1, x2, y1 e y2");
-    scanf("%d%d%d%d", &x1,&x2,&y1,&y2);
+    printf("Ingrese x1");
+    scanf("%d", &x1);
+    printf("Ingrese x2");
+    scanf("%d", &x2);
+    printf("Ingrese y1");
+    scanf("%d", &y1);
+    printf("Ingrese y2");
+    scanf("%d", &y2);
     resultado = ((((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))));
     printf("La diferencia entre los puntos es = %d", resultado);
     printf("\n");
