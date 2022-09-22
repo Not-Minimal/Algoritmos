3246c6b02180054c5788f7b14229da0b430c7f64
Saul Muñoz <58341096+Not-Minimal@users.noreply.github.com>
2022-09-22 16:56:40 -0300

    Update ejercicio1.c

--- Segundo Semestre/Ejercicios/ejercicio1.c
+++ Segundo Semestre/Ejercicios/ejercicio1.c
@@ -1,1 +1,14 @@
-Hola
+#include<stdio.h>
+//Definicion de variables globales
+int a;
+int b;
+
+int main()
+{
+    printf("Ingrese el valor del producto: ");
+    scanf("%d",&a);
+    b = (a*0.19);
+    printf("El iva del producto es = %d", b);
+    printf("\n");
+    return 0;
+}
