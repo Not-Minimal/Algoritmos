c5f5fe9d781f103b3dbfb8fba1cd967cb2b50122
Saul Muñoz <58341096+Not-Minimal@users.noreply.github.com>
2022-09-22 15:54:15 -0300

    Create PrimeraClase.C
    Contenido visto en la primera clase del dia 22/09/2022

---
+++ Segundo Semestre/Clases/PrimeraClase.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// Definicion de Variables Globales
+int a, b, s;
+int main(){
+    //Definicion de Variables locales
+    printf("Ingrese valores a sumar: ");
+    scanf("%d%d", &a,&b);
+    s = a + b;
+    printf("El resultado de suma = %d", s);
+    printf("\n");
+    return 0;
+}
