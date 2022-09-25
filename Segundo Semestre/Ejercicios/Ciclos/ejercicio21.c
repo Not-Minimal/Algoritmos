81e2d0f59d86bc60a7dbae9b60a0bd76d48c61cb
Saul Muñoz <sam.pedreros14@gmail.com>
2022-09-25 19:32:21 -0300

    Correcion

--- Segundo Semestre/Ejercicios/Ciclos/ejercicio21.c
+++ Segundo Semestre/Ejercicios/Ciclos/ejercicio21.c
@@ -11,7 +11,7 @@ int main(int argc, char const *argv[])
         scanf("%d", &a);
         printf("Ingrese el segundo termino: ");
         scanf("%d", &b);
-    } while ((b > a) && ((a != 0)&&(b != 0)));
+    } while ((b > a) || ((a = 0) || (b = 0)));
     u = 0;
     i = 1;
 
@@ -22,6 +22,7 @@ int main(int argc, char const *argv[])
         i++;
         resultado = (resultado - b);
         printf("%d", i);
+        printf("\n");
     } while (b > i);
     printf("\n");
     return 0;
