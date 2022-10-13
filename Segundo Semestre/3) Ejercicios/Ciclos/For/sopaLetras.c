9a10a7f9e437b2f511d0c5e89742f323f50a51a4
Saul Muñoz <sam.pedreros14@gmail.com>
2022-10-13 08:59:51 -0300

    Actualizar codigo

--- Segundo Semestre/3) Ejercicios/Ciclos/For/sopaLetras.c
+++ Segundo Semestre/3) Ejercicios/Ciclos/For/sopaLetras.c
@@ -1,62 +1,71 @@
-//Edades aleatorias y mayor o menor de edad del rango.
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
+// Edades aleatorias y mayor o menor de edad del rango.
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int n, i, j, filas, columnas, opcion;
 int main(int argc, char const *argv[])
 {
-    //Variables Tiempo
+    // Variables Tiempo
     time_t t;
     clock_t inicio, fin;
     double tiempo;
 
-    system ("Clear");
-    printf("Ingrese el tamaño de la Sopa de Letras "); 
-    scanf("%d", &n);
-    filas = n;
-    columnas = n;
-    char matriz[filas][columnas];
-    srand(time(NULL));
-    for (i = 0; i < filas; i++) //Relleno el arreglo desde la posicion 0 hasta n-1
+    do
     {
-        for (j = 0; j < columnas; j++)
+        printf("Opciones: ");
+        printf("\n");
+        printf("1. Crear una Sopa de Letras de Tamaño NxN");
+        printf("\n");
+        printf("2. Esconder una palabra");
+        printf("\n");
+        printf("3. Esconder dos palabras");
+        printf("\n");
+        printf("4. Salir");
+        printf("\n");
+        printf("Ingrese la opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion)
         {
-           matriz[i][j] = rand() % 26 + 65;
-        }        
-    }
-    for (i = 0; i < filas; i++) //Relleno el arreglo desde la posicion 0 hasta n-1
-    {
-        for (j = 0; j < columnas; j++)
-        {
-           printf("%c  ",matriz[i][j]);
-           
-        }   
-        printf("\n");     
-    }
+        case 1:
+            inicio = clock();
+            system("Clear");
+            printf("Ingrese el tamaño de la Sopa de Letras ");
+            scanf("%d", &n);
+            filas = n;
+            columnas = n;
+            char matriz[filas][columnas];
+            srand(time(NULL));
+            for (i = 0; i < filas; i++) // Relleno el arreglo desde la posicion 0 hasta n-1
+            {
+                for (j = 0; j < columnas; j++)
+                {
+                    matriz[i][j] = rand() % 26 + 65;
+                }
+            }
+            for (i = 0; i < filas; i++) // Relleno el arreglo desde la posicion 0 hasta n-1
+            {
+                for (j = 0; j < columnas; j++)
+                {
+                    printf("%c  ", matriz[i][j]);
+                }
+                printf("\n");
+            }
 
-    printf("Opciones: ");
-    printf("\n");
-    printf("1.- Encontre la palabra.");
-    printf("\n");
-    printf("2.- Sigo sin encontrar la palabra.");
-    printf("\n");
-    
-    scanf("%d", &opcion);
+            printf("Presione 1 si encontro la palabra: ");
+            scanf("%d", &opcion);
+            if (opcion)
+            {
+                fin = clock();
+                tiempo = ((double)(fin - inicio)) / CLOCKS_PER_SEC;
+                printf("El tiempo que tomo en encontrar la palabra es de: %.2f", tiempo*10000);
+                printf("\n");
+            }
+            break;
+        }
 
-    inicio = clock();
-    if (opcion == 1)
-    {
-        
-    } 
-    else if (opcion == 2)
-    {
-        
-    } 
+    } while (opcion < 1 && opcion > 4);
 
-    fin = clock();
-        tiempo = ((double) (fin - inicio)) / CLOCKS_PER_SEC;
-        printf("El tiempo que tomo en encontrar la palabra es de: %f", tiempo);
-    
     return 0;
 }
