1098722d8e588b23c68be270f52734f1727e2e8d
Saul Muñoz <sam.pedreros14@gmail.com>
2022-10-20 09:15:43 -0300

    Funciona

--- Segundo Semestre/4) Tareas/SopaLetras.c
+++ Segundo Semestre/4) Tareas/SopaLetras.c
@@ -87,7 +87,7 @@ int main(int argc, char const *argv[])
                             printf("Ingrese la palabra N° %d", i + 1);
                             printf("\n");
                             scanf("%s", palabra);
-                        } while ((strlen(palabra) > 1) || (strlen(palabra) < n));
+                        } while ((strlen(palabra) < 1) || (strlen(palabra) > n));
 
                         for (j = 0; j < k && palabra[j] != NULL; j++)
                         {
