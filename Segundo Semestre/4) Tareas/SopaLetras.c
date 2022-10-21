dc34b691612147307b982f9c31cc31e724b0c55d
Saul Muñoz <sam.pedreros14@gmail.com>
2022-10-20 21:31:50 -0300

    eliminar int

--- Segundo Semestre/4) Tareas/SopaLetras.c
+++ Segundo Semestre/4) Tareas/SopaLetras.c
@@ -90,7 +90,7 @@ int main(int argc, char const *argv[])
                             printf("Ingrese la palabra N° %d", i + 1);
                             printf("\n");
                             scanf("%s", palabra);
-                            int aux = palabra;
+                            aux = palabra;
                         } while ((strlen(palabra) < 1) || (strlen(palabra) > n));
 
                         for (j = 0; j < aux && palabra[j] != '\0'; j++)
