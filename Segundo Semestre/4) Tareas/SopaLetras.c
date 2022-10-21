f5d6d795be66cf786ce8b3c37a330c1b2df59b8c
Saul Muñoz <sam.pedreros14@gmail.com>
2022-10-20 21:38:32 -0300

    Todo bien

--- Segundo Semestre/4) Tareas/SopaLetras.c
+++ Segundo Semestre/4) Tareas/SopaLetras.c
@@ -90,10 +90,11 @@ int main(int argc, char const *argv[])
                             printf("Ingrese la palabra N° %d", i + 1);
                             printf("\n");
                             scanf("%s", palabra);
+                            
                         } while ((strlen(palabra) < 1) || (strlen(palabra) > n));
-                   
+                        aux = n;
 
-                        for (j = 0; j < palabra && palabra[j] != '\0'; j++)
+                        for (j = 0; j < aux && palabra[j] != '\0'; j++)
                         {
                             matriz[i][j] = palabra[j];
                         }
@@ -138,6 +139,7 @@ int main(int argc, char const *argv[])
                                 }
                                 printf("\n");
                             }
+                            break;
                         }
                         case 2:
                         {
