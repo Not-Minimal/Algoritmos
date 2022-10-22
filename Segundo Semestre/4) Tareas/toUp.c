e96cfa9f20e697a01e11f8ed2a28367c95865088
Saul Muñoz <sam.pedreros14@gmail.com>
2022-10-22 15:25:36 -0300

    Quitar print extra

--- Segundo Semestre/4) Tareas/toUp.c
+++ Segundo Semestre/4) Tareas/toUp.c
@@ -9,25 +9,17 @@ int main(int argc, char const *argv[])
     {
         for (j = 0; j < 1;j++)
         {
-           printf("Ingrese palabra: ");
+           printf("Ingrese palabra: N°%d ", i+1);
            scanf("%s", &palabra);
         }
         
-    }
-    for (i = 0; i < 5; i++) // Muestro la matriz con las palabras
-    {
-        for (j = 0; j < 1; j++)
-        {
-            printf("[%c] ", matriz[i][j]);
-        }
-        printf("\n");
     }
 
     // Convertir cada char a mayúscula
     // usando toupper
     for (int i = 0; matriz[i] != '\0'; ++i)
     {
-        for (j = 0; j < 1; j++)
+        for (j = 0; matriz[j] != '\0'; j++)
         {
             matriz[i][j] = toupper(matriz[i][j]);
         }
