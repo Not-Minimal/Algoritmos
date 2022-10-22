f110ec0c7dc586b3a5e594480c5f04b7f2388693
Saul Muñoz <sam.pedreros14@gmail.com>
2022-10-22 15:38:18 -0300

    Funiciona

--- Segundo Semestre/4) Tareas/toUp.c
+++ Segundo Semestre/4) Tareas/toUp.c
@@ -1,39 +1,22 @@
 #include <stdio.h>
 #include <ctype.h>
-int i, j;
+
 int main(int argc, char const *argv[])
 {
-    char palabra[5];
-    char matriz[5][5];
-    for (i = 0; i < 5; i++)
+
+    char palabra[10];
+    for (int i = 0; i < 5; i++)
     {
-        for (j = 0; j < 1;j++)
-        {
-           printf("Ingrese palabra: N°%d ", i+1);
-           scanf("%s", &palabra);
-        }
-        
+        printf("Ingrese la palabra: ");
+        scanf("%s", palabra);
     }
 
     // Convertir cada char a mayúscula
     // usando toupper
-    for (int i = 0; matriz[i] != '\0'; ++i)
+    for (int i = 0; palabra[i] != '\0'; ++i)
     {
-        for (j = 0; matriz[j] != '\0'; j++)
-        {
-            matriz[i][j] = toupper(matriz[i][j]);
-        }
-        
+        palabra[i] = toupper(palabra[i]);
     }
-
-    for (i = 0; i < 5; i++) // Muestro la matriz con las palabras
-    {
-        for (j = 0; j < 1; j++)
-        {
-            printf("[%c] ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-    
+    printf("%s\n", palabra);
     return 0;
 }
