3c31ccaa37a4966b22a5d4cf40f511a6abdbb3cd
Saul Muñoz <sam.pedreros14@gmail.com>
2022-10-22 16:02:03 -0300

    Actualizar

--- Segundo Semestre/4) Tareas/toUp.c
+++ Segundo Semestre/4) Tareas/toUp.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
-
+int i, j, k;
 int main(int argc, char const *argv[])
 {
 
     char palabra[10];
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Ingrese la palabra: ");
-        scanf("%s", palabra);
-    }
 
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; palabra[j] != '\0'; ++j)
-        {
-            palabra[j] = toupper(palabra[j]);
-        }
-    }
+    printf("Ingrese la palabra: ");
+    scanf("%s", palabra);
 
+    for (int i = 0; palabra[i] != '\0'; ++i)
+    {
+        palabra[i] = toupper(palabra[i]);
+    }
     printf("%s\n", palabra);
+
     return 0;
 }
