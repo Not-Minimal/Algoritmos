408254f0ea449496bd5419d7e8c2f877fd707ec7
Saul Muñoz <sam.pedreros14@gmail.com>
2022-10-23 20:07:40 -0300

    Todas las funciones de orden ok

--- Segundo Semestre/4) Tareas/ordenAleatorio.c
+++ Segundo Semestre/4) Tareas/ordenAleatorio.c
@@ -26,6 +26,24 @@ void pasarHorizontal(char matriz[filas][columnas], char palabra[], int f, int c)
     }
 }
 
+void pasarHorizontalInverso(char matriz[filas][columnas], char palabra[], int f, int c)
+{
+    int k = 0;
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matriz[i][j] = 32;
+        }
+    }
+
+    for (int i = strlen(palabra) - 1; i >= 0; i--)
+    {
+        matriz[f][c + k] = palabra[i];
+        k++;
+    }
+}
+
 void pasarVertical(char matriz[filas][columnas], char palabra[], int f, int c)
 {
 
@@ -40,6 +58,23 @@ void pasarVertical(char matriz[filas][columnas], char palabra[], int f, int c)
     for (int i = 0; i < strlen(palabra); i++)
     {
         matriz[f + i][c] = palabra[i];
+    }
+}
+void pasarVerticalInverso(char matriz[filas][columnas], char palabra[], int f, int c)
+{
+    int k = 0;
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matriz[i][j] = 32;
+        }
+    }
+
+    for (int i = strlen(palabra) - 1; i >= 0; i--)
+    {
+        matriz[f + k][c] = palabra[i];
+        k++;
     }
 }
 
@@ -58,18 +93,12 @@ void visualizaerMatriz(char matriz[filas][columnas])
 
 int main(int argc, char const *argv[])
 {
-
-    
     char matriz[filas][columnas];
-
     printf("Ingrese palabra: ");
     printf("\n");
     scanf("%s", palabra);
-
     srand(time(NULL));
-
-    pasarVertical(matriz, palabra, rand() % filas, rand() % columnas);
+    pasarVerticalInverso(matriz, palabra, rand() % filas, rand() % columnas);
     visualizaerMatriz(matriz);
-
     return 0;
 }
