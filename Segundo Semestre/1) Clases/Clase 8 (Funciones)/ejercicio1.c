21fbf7e9afa9abf4081a0b63c19baa6456558a30
Saul Muñoz <sam.pedreros14@gmail.com>
2022-11-02 23:33:42 -0300

    Actualziar ejercico PPT

---
+++ Segundo Semestre/1) Clases/Clase 8 (Funciones)/ejercicio1.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+// Funciones
+// int exponente();
+float factorial();
+long potencia();
+
+// Variables Globales
+int base, k;
+
+float factorial(int numero)
+{
+    int i, fx;
+    fx = 1;
+    if (numero == 0)
+    {
+        return numero;
+    }
+    else
+    {
+        for (int i = 1; i <= numero; i++)
+        {
+            fx = i * fx;
+        }
+        return (fx);
+    }
+}
+
+long potencia(int base, int potencia)
+{
+    long resultado = base;
+    if (base == 0)
+    {
+        return base;
+    }
+    else
+    {
+        while (potencia > 1)
+        {
+            resultado = resultado * base;
+            potencia--;
+        }
+        return (resultado);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int x, b;
+    float resultado;
+    long resultadoPot;
+    printf("\nEvaluar F(x)");
+    printf("\nIngrese el valor de x: ");
+    scanf("%d", &x);
+    if (x > 10)
+    {
+        resultado = factorial(x);
+        printf("El resultado de F(%d) es: %2.f \n", x, resultado);
+    }
+    else if (x >= 5 && x <= 10)
+    {
+        printf("Para este calculo necesita un valor extra (b).");
+        printf("\nIngrese el valor de b: ");
+        scanf("%d", &b);
+        resultado = factorial(x + b);
+        resultadoPot = potencia(resultado, x);
+        printf("El resultado de F(%d)! elevado a %d es: %ld \n", (x + b), x, resultadoPot);
+    }
+    else if (x > 0 && x < 5)
+    {
+        printf("Para este calculo necesita un valor extra (b).");
+        printf("\nIngrese el valor de b: ");
+        scanf("%d", &b);
+        // resultado = factorial(x);
+        resultadoPot = potencia(x, factorial(b - x));
+        if ((b - x) == 0)
+        {
+            printf("El resultado de F(%d)! elevado a %d es: %d \n", x, 0, 1);
+        }
+        else
+        {
+            printf("El resultado de F(%d)! elevado a %d es: %ld \n", x, (b - x), resultadoPot);
+        }
+    }
+    return 0;
+}
