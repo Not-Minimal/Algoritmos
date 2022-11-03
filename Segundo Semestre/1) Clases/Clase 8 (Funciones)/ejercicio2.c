5ffe429f71f6414f82d09ef746269d27d7fd1071
Saul Muñoz <sam.pedreros14@gmail.com>
2022-11-03 00:01:32 -0300

    act

--- Segundo Semestre/1) Clases/Clase 8 (Funciones)/ejercicio2.c
+++ Segundo Semestre/1) Clases/Clase 8 (Funciones)/ejercicio2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+void cadenas(char palabra[], int f, int c);
 
 int i, j, k, filas = 10, columnas = 10, n, cantidadPalabras, posicionPalabra;
 char palabra[10];
@@ -19,14 +23,14 @@ void cadenas(char palabra[], int f, int c)
 
 int main(int argc, char const *argv[])
 {
-    system("Clear");
+    system("Clr");
     for (i = 0; i < 2; i++)
     {
-        printf("Ingrese palabra N°%d: ", cantidadPalabras + 1);
+        printf("Ingrese palabra N°%d: ", i + 1);
         printf("\n");
         scanf("%s", palabra);
 
-        cadenas(palabra);
+        cadenas(palabra, 1, 1);
     }
 
     return 0;
