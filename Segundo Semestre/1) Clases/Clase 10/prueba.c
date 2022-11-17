38c30bde2db462c3b8564a7700e22eab8313ffa1
Saul Muñoz <sam.pedreros14@gmail.com>
2022-11-17 10:48:11 -0300

    act

--- Segundo Semestre/1) Clases/Clase 10/prueba.c
+++ Segundo Semestre/1) Clases/Clase 10/prueba.c
@@ -1,14 +1,65 @@
-#include <stdio.h>
-#include <math.h>
+/*  Author: Alejandro */
+/*  E-mail: farias_io@pm.me */
 
-int main(int argc, char const *argv[])
+#include <stdio.h>
+
+int longitud(int rut)
 {
-    int m = 0, s = 1;
-    for (int t = 0; t < floor(t / 10); t++)
+    int contador = 0;
+    while (rut > 0)
     {
-        s = (s + t % 10 * (9 - m++ % 6)) % 11;
+        rut = rut / 10;
+        contador++;
+    }
+    return contador;
+}
+
+int calculoDigito(int rut)
+{
+    /* Variables locales */
+    int largo = longitud(rut), val[largo], contador = 2, suma = 0, resta2, i, mod_f, resta;
+    float mod;
+    /* Fin variables locales */
+
+    for (i = 0; i <= largo; i++)
+    {
+        val[i] = rut % 10 * contador;
+        contador = contador + 1;
+        if (contador == 8)
+        {
+            contador = 2;
+        }
+        rut = rut / 10;
     }
 
-    printf("%d", s);
+    for (i = 0; i < largo; i++)
+    {
+        suma = suma + val[i];
+    }
+
+    mod = suma / 11;      // Se obtiene el modulo
+    mod_f = mod * 11;    // Se multiplica el modulo * 11
+    resta = suma - mod_f; // Se resta la sumaatoria - el modulo
+    resta2 = 11 - resta;    // Se resta 11 - la diferencia de la sumaatoria - el modulo
+    return resta2;
+}
+
+int main(void)
+{
+    int rut, digito;
+    printf("Calcular digito verificador RUT\n");
+    printf("Ingrese el RUT sin digito: ");
+    scanf("%d", &rut);
+    digito = calculoDigito(rut);
+    if (digito == 10)
+    {
+        printf("El digito verificador es: K\n");
+        printf("El RUT completo es: %d-K\n", rut);
+    }
+    else
+    {
+        printf("El digito vertificador es: %d\n", digito);
+        printf("El RUT completo es: %d-%d\n", rut, digito);
+    }
     return 0;
 }
