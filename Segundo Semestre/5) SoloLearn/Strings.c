388771d5b9c28b94fa4d15bbbecc6fcd731d8890
Saul Muñoz <sam.pedreros14@gmail.com>
2022-11-20 17:08:59 -0300

    act

--- Segundo Semestre/5) SoloLearn/Strings.c
+++ Segundo Semestre/5) SoloLearn/Strings.c
@@ -83,4 +83,17 @@ int main()
 
 The puts() function takes only a string argument and can also be used to display output. However, it adds a newline to output.
 For example:
+#include <stdio.h>
+int main()
+{
+  char city[40];
+  printf("Enter your favorite city: ");
+  gets(city);
+  // Note: for safety, use
+  // fgets(city, 40, stdin);
+
+  puts(city);
+
+  return 0;
+} 
 
