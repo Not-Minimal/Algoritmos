e07c34b916713179431f1179b707b05b5a82c7fe
Saul Muñoz <sam.pedreros14@gmail.com>
2022-11-20 17:08:31 -0300

    Act

--- Segundo Semestre/5) SoloLearn/Strings.c
+++ Segundo Semestre/5) SoloLearn/Strings.c
@@ -67,6 +67,19 @@ String Output
 String output is handled with the fputs(), puts(), and printf() functions.
 The fputs() requires the name of the string and a pointer to where you want to print the string. To print to the screen, use stdout which refers to the standard output.
 For example:
+#include <stdio.h>
+int main()
+{
+  char city[40];
+  printf("Enter your favorite city: ");
+  gets(city);
+  // Note: for safety, use
+  // fgets(city, 40, stdin);
+  fputs(city, stdout);
+  printf(" is a fun city.");
+
+  return 0;
+} 
 
 The puts() function takes only a string argument and can also be used to display output. However, it adds a newline to output.
 For example:
