a7f4ef231949fb2313c3eae5c9fa1f15ac8c53d7
Saul Muñoz <sam.pedreros14@gmail.com>
2022-12-19 20:01:21 -0300

    Agregar Prototipos

--- Segundo Semestre/4) Tareas/Tarea 2/Prueba.c
+++ Segundo Semestre/4) Tareas/Tarea 2/Prueba.c
@@ -3,3 +3,11 @@
 #include <stdlib.h>
 #include <conio.h>
 
+//Agregar Prototipos de Funciones
+void usuario_Login();
+void agregar_Producto();
+void actualizar_Producto();
+void buscar_Productos();
+void eliminar_Productos();
+
+
