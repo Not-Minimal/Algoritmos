44c1b3b6babdd87e3f385870bdcf3d57055a7777
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-01 16:27:59 -0300

    Update

--- Segundo Semestre/3) Ejercicios/Basicos/ejercicio1.c
+++ Segundo Semestre/3) Ejercicios/Basicos/ejercicio1.c
@@ -1,14 +1,16 @@
-#include<stdio.h>
-//Definicion de variables globales
+#include <stdio.h>
+// Definicion de variables globales
 int a;
 int b;
 
-int main()
-{
-    printf("Ingrese el valor del producto: ");
-    scanf("%d",&a);
-    b = (a*0.19);
-    printf("El iva del producto es = %d", b);
-    printf("\n");
-    return 0;
+int main() {
+  printf("Ingrese el valor del producto: ");
+  scanf("%d", &a);
+  b = (a * 0.19);
+  printf("El iva del producto es = %d", b);
+  printf("\n");
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", &i);
+  }
+  return 0;
 }
