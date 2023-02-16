c1e5d465da6d91e11f826a4a7a454cc0c411c100
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-16 13:07:55 -0300

    Recursividad

---
+++ Segundo Semestre/6) Recursividad/Certamen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int sumatoria(int n, int i);
+
+int sumatoria(int n, int i)
+{
+
+    if (n == i)
+    {
+        return n;
+    }
+    else
+    {
+        int resultado = (((3 * i) + 1) / (2 * i));
+        return i + sumatoria(n, resultado + 1);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Ingrese n: ");
+    scanf("%d", &n);
+    printf("%d \n", sumatoria(n, 1));
+    return 0;
+}
